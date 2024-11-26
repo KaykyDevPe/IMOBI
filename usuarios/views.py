01ef97eab0cc61a12bd2d78a557852ec787647a9
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-11-26 08:12:12 -0300

    01 - IMOBI - 58:08

---
+++ usuarios/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def logar(request):
+    pass
