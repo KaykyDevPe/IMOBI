ef4dc06986dd2ab7badc8d562755b91d716b0fd6
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-11-27 08:03:34 -0300

    01 - IMOBI - 44:00

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/auth/cadastro')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse nome cadastrado')
+            return redirect('/auth/cadastro')
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+            return redirect('/auth/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/auth/cadastro')
 
 def logar(request):
-    pass
+    return render(request, 'logar.html')
