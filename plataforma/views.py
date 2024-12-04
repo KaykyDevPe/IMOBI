45a2dd5b22b6ab3c13b109a3aea508b4a7f84226
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-12-04 08:03:18 -0300

    02 - IMOBI - 45:58

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from . models import Imovel
+from . models import Imovel, Cidade
 
 @login_required(login_url='/auth/logar/')
 def home(request):
+    cidades = Cidade.objects.all()
     imoveis = Imovel.objects.all()
-    return render(request, 'home.html', {'imoveis': imoveis})
+    
+    context = {
+        'cidades': cidades,
+        'imoveis': imoveis
+    }
+    
+    return render(request, 'home.html', context)
