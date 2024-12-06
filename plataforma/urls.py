c25092b1021a13003f6bbe7ad0bb29f0488c49f6
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-12-06 07:49:53 -0300

    02 - IMOBI - FINALIZADA

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('imovel/<str:id>', views.imovel, name="imovel"),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
